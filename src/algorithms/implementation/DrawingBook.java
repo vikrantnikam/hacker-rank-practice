657089b1b65d47bb4855a66d3994e7783b47f425
Vikrant Nikam <vikrant.nikam@dektech.com.au>
2018-05-02 21:39:21 +0200

    Algorithm - Drawing Book Solution

---
+++ src/algorithms/implementation/DrawingBook.java
@@ -0,0 +1,40 @@
+package algorithms.implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class DrawingBook {
+
+    /*
+     * Complete the pageCount function below.
+     */
+    static int pageCount(int n, int p) {
+        int fromStart = Math.abs(p/2);
+        if(n%2==0) n++;
+        int fromEnd = Math.abs((n-p)/2);
+        return fromStart < fromEnd ? fromStart : fromEnd;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])*");
+
+        int result = pageCount(n, p);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
