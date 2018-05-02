39c00cf374e8e9c9b7e881073340e051be3d094a
Vikrant Nikam <vikrant.nikam@dektech.com.au>
2018-05-02 20:39:56 +0200

    Algorithm - A Very Big Sum solution

---
+++ src/algorithms/warmup/VeryBigSum.java
@@ -0,0 +1,45 @@
+package algorithms.warmup;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class VeryBigSum {
+
+	/*
+	 * Complete the aVeryBigSum function below.
+	 */
+	static long aVeryBigSum(int n, long[] ar) {
+		/*
+		 * Write your code here.
+		 */
+		long sum = 0;
+		for (long a : ar) {
+			sum += a;
+		}
+		return sum;
+	}
+
+	private static final Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		
+		int n = Integer.parseInt(scan.nextLine().trim());
+
+		long[] ar = new long[n];
+
+		String[] arItems = scan.nextLine().split(" ");
+
+		for (int arItr = 0; arItr < n; arItr++) {
+			long arItem = Long.parseLong(arItems[arItr].trim());
+			ar[arItr] = arItem;
+		}
+
+		long result = aVeryBigSum(n, ar);
+
+		System.out.println(result);
+		
+		scan.close();
+	}
+}
