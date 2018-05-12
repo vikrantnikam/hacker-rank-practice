9dfe1d223d8236a6819c7bb7c21ddb9a89dfee54
Vikrant Nikam <vikrant.nikam@dektech.com.au>
2018-05-12 22:17:26 +0200

    Solution updated

--- src/algorithms/implementation/DayOfProgrammer.java
+++ src/algorithms/implementation/DayOfProgrammer.java
@@ -1,50 +1,49 @@
-import java.io.*;
-import java.math.*;
-import java.security.*;
-import java.text.*;
-import java.util.*;
-import java.util.concurrent.*;
-import java.util.regex.*;
+package algorithms.implementation;
 
-public class Solution {
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
 
-    // Complete the solve function below.
-    static String solve( int year ) {
+public class DayOfProgrammer {
 
-        if( year == 1918 ) 
-            return "26.09.1918";
-        if( isLeap( year ) ) 
-            return "12.09." + Integer.toString( year );
-        else 
-            return "13.09." + Integer.toString( year );
+	// Complete the solve function below.
+	static String solve(int year) {
 
-    }
+		if (year == 1918)
+			return "26.09.1918";
+		if (isLeap(year))
+			return "12.09." + Integer.toString(year);
+		else
+			return "13.09." + Integer.toString(year);
 
-    static boolean isLeap( int year ) {
-        boolean isLeap = true;
-        if( year % 4 != 0 ) 
-            isLeap = false;
-        if( year > 1918 && year % 100 == 0 && year % 400 != 0 ) 
-            isLeap = false;
-        return isLeap;
+	}
 
-    }
+	static boolean isLeap(int year) {
+		boolean isLeap = true;
+		if (year % 4 != 0)
+			isLeap = false;
+		if (year > 1918 && year % 100 == 0 && year % 400 != 0)
+			isLeap = false;
+		return isLeap;
 
-    private static final Scanner scanner = new Scanner(System.in);
+	}
 
-    public static void main(String[] args) throws IOException {
-        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+	private static final Scanner scanner = new Scanner(System.in);
 
-        int year = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String result = solve(year);
+		int year = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
-        bufferedWriter.write(result);
-        bufferedWriter.newLine();
+		String result = solve(year);
 
-        bufferedWriter.close();
+		bufferedWriter.write(result);
+		bufferedWriter.newLine();
 
-        scanner.close();
-    }
+		bufferedWriter.close();
+
+		scanner.close();
+	}
 }
