aa39662de5f7482877fa7a53e3be46aeddc7f878
Vikrant Nikam <vikrant.nikam@dektech.com.au>
2018-05-21 19:25:02 +0200

    Angry professor solution

---
+++ src/algorithms/implementation/AngryProfessor.java
@@ -0,0 +1,55 @@
+package algorithms.implementation;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class AngryProfessor {
+
+	// Complete the angryProfessor function below.
+	static String angryProfessor(int k, int[] a) {
+		int studentsOnTime = 0;
+		for (int n : a) {
+			if (n <= 0)
+				studentsOnTime++;
+		}
+		return (studentsOnTime >= k) ? "NO" : "YES";
+	}
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+		int t = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+		for (int tItr = 0; tItr < t; tItr++) {
+			String[] nk = scanner.nextLine().split(" ");
+
+			int n = Integer.parseInt(nk[0]);
+
+			int k = Integer.parseInt(nk[1]);
+
+			int[] a = new int[n];
+
+			String[] aItems = scanner.nextLine().split(" ");
+			scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+			for (int i = 0; i < n; i++) {
+				int aItem = Integer.parseInt(aItems[i]);
+				a[i] = aItem;
+			}
+
+			String result = angryProfessor(k, a);
+
+			bufferedWriter.write(result);
+			bufferedWriter.newLine();
+		}
+
+		bufferedWriter.close();
+
+		scanner.close();
+	}
+}
