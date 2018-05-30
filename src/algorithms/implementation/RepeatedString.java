6166b0abb81f92c0c26f7afd96360f2681784b2e
vikrantnikam <38431993+vikrantnikam@users.noreply.github.com>
2018-05-30 21:07:25 +0200

    Create RepeatedString.java
    Algorithm - Repeated string solution

---
+++ src/algorithms/implementation/RepeatedString.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long count = s.chars().filter(ch -> ch == 'a').count();
+        long repitiotions = (n/s.length());
+        long remainder = (n%s.length());
+        count =  repitiotions*count;
+        count += s.substring(0,(int)remainder).chars().filter(ch -> ch == 'a').count();
+        return count;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
